4cc657f4d3b1c86bc1122afa68eff5d9040b85fe
VIVEK ANAND <47303600+imvivekanand@users.noreply.github.com>
2023-07-09 23:25:13 +0530

    Todo list working

--- app.js
+++ app.js
@@ -3,39 +3,32 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+let items = [];
+
 app.set("view engine", "ejs");
 
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.get("/", function (req, res) {
-  var today = new Date();
-  var currentDay = today.getDay();
-  var day = "";
+  let today = new Date();
 
-  switch (currentDay) {
-    case 0:
-      day = "Sunday";
-    case 1:
-      day = "Monday";
-      break;
-    case 2:
-      day = "Tuesday";
-      break;
-    case 3:
-      day = "Wednesday";
-      break;
-    case 4:
-      day = "Thursday";
-      break;
-    case 5:
-      day = "Friday";
-      break;
-    case 6:
-      day = "Saturday";
-      break;
-      default:
-        console.log("Error: current day is equal to: " + currentDay);
-  }
+  let options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long",
+  };
 
-  res.render("list", { kindOfDay: day });
+  let day = today.toLocaleDateString("hi-IN", options);
+
+  res.render("list", { kindOfDay: day, newListItems: items });
+});
+
+app.post("/", function (req, res) {
+  let item = req.body.newItem;
+
+  items.push(item);
+
+  res.redirect("/");
 });
 
 app.listen(3000, function (req, res) {
